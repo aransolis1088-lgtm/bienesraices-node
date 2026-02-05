c532bfd25ea4a3b6a1a3ada8b1052a2c004811c3
Aran Uzziel Solis Medina <aran.solis1088@gmail.com>
2026-02-04 22:25:31 -0600

    Interfaz de ediciónn de propiedades

--- controllers/propiedadController.js
+++ controllers/propiedadController.js
@@ -8,10 +8,10 @@ const admin = async (req, res) => {
       usuarioId: id,
     },
     include: [
-      {model: Categoria, as: 'categoria'}, 
-      {model: Precio, as: 'precio'}, 
-    ]
-  })
+      { model: Categoria, as: "categoria" },
+      { model: Precio, as: "precio" },
+    ],
+  });
 
   res.render("propiedades/admin", {
     pagina: "Mis Propiedades",
@@ -150,4 +150,103 @@ const almacenarImagen = async (req, resp, next) => {
   }
 };
 
-export { admin, crear, guardar, agregarImagen, almacenarImagen };
+const editar = async (req, res) => {
+  const { id } = req.params;
+
+  //Validar que la propiedad exista
+  const propiedad = await Propiedad.findByPk(id);
+
+  if (!propiedad) {
+    return res.redirect("/mis-propiedades");
+  }
+
+  //Revisar que usuario quien consulta es quien creo propiedad
+  if (propiedad.usuarioId.toString() !== req.usuario.id.toString()) {
+    return res.redirect("/mis-propiedades");
+  }
+
+  const [categorias, precios] = await Promise.all([
+    Categoria.findAll(),
+    Precio.findAll(),
+  ]);
+  res.render("propiedades/editar", {
+    pagina: `Editar Propiedad: ${propiedad.titulo}`,
+    csrFToken: req.csrfToken(),
+    categorias,
+    precios,
+    datos: propiedad,
+  });
+};
+
+const guardarCambios = async (req, res) => {
+  //verificar la validación
+  let resultado = validationResult(req);
+  if (!resultado.isEmpty()) {
+    //Consultar modelo de precio y categorias
+    const [categorias, precios] = await Promise.all([
+      Categoria.findAll(),
+      Precio.findAll(),
+    ]);
+
+    // Errores
+    return res.render("propiedades/editar", {
+      pagina: "Editar Propiedad",
+      csrFToken: req.csrfToken(),
+      categorias,
+      precios,
+      errores: resultado.array(),
+      datos: req.body,
+    });
+  }
+
+  const { id } = req.params;
+
+  //Validar que la propiedad exista
+  const propiedad = await Propiedad.findByPk(id);
+
+  if (!propiedad) {
+    return res.redirect("/mis-propiedades");
+  }
+
+  //Revisar que usuario quien consulta es quien creo propiedad
+  if (propiedad.usuarioId.toString() !== req.usuario.id.toString()) {
+    return res.redirect("/mis-propiedades");
+  }
+
+  //Reescribir el objeto y actualizarlo
+
+  try{
+      const {titulo,descripcion,habitaciones,estacionamiento,wc,calle,latitud,longitud,precio: precioId,categoria: categoriaId,} = req.body;
+
+      propiedad.set({
+        titulo,
+        descripcion,
+        habitaciones,
+        estacionamiento,
+        wc,
+        calle,
+        latitud,
+        longitud,
+        precioId,
+        categoriaId
+      })
+
+      await propiedad.save();
+
+      res.redirect('/mis-propiedades')
+
+  }catch(error){
+    console.log(error)
+  }
+
+};
+
+export {
+  admin,
+  crear,
+  guardar,
+  agregarImagen,
+  almacenarImagen,
+  editar,
+  guardarCambios,
+};
--- routes/propiedadesRoutes.js
+++ routes/propiedadesRoutes.js
@@ -5,10 +5,12 @@ import {
   crear,
   guardar,
   agregarImagen,
-  almacenarImagen
+  almacenarImagen,
+  editar,
+  guardarCambios
 } from "../controllers/propiedadController.js";
 import protegerRuta from "../middleware/protegerRuta.js";
-import upload  from "../middleware/subirImagen.js";
+import upload from "../middleware/subirImagen.js";
 
 const router = express.Router();
 
@@ -39,6 +41,35 @@ router.post(
 
 router.get("/mis-propiedades/agregar-imagen/:id", protegerRuta, agregarImagen);
 
-router.post("/mis-propiedades/agregar-imagen/:id", protegerRuta, upload.single("imagen"), almacenarImagen);
+router.post(
+  "/mis-propiedades/agregar-imagen/:id",
+  protegerRuta,
+  upload.single("imagen"),
+  almacenarImagen,
+);
+
+router.get("/propiedades/editar/:id", protegerRuta, editar);
+
+router.post("/propiedades/editar/:id",
+  protegerRuta,
+  body("titulo").notEmpty().withMessage("El titulo es obligatorio"),
+  body("descripcion")
+    .notEmpty()
+    .withMessage("La descripcion es obligatoria")
+    .isLength({ max: 200 })
+    .withMessage("La descripcion no puede ser mayor a 200 caracteres"),
+  body("habitaciones")
+    .isNumeric()
+    .withMessage("Las habitaciones son obligatorias"),
+  body("categoria").isNumeric().withMessage("La categoria es obligatoria"),
+  body("precio").isNumeric().withMessage("El precio es obligatorio"),
+  body("estacionamiento")
+    .notEmpty()
+    .withMessage("El numero de estacionamientos es obligatorio"),
+  body("wc").notEmpty().withMessage("El numero de baños es obligatorio"),
+  body("latitud").notEmpty().withMessage("La latitud es obligatoria"),
+
+  guardarCambios,
+);
 
 export default router;
