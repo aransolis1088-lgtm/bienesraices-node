c8fda21f46cf03649aacc0c41d68cfbaf9cc7b2b
Aran Uzziel Solis Medina <aran.solis1088@gmail.com>
2026-02-11 22:47:33 -0600

    Mostrar casas y departamentos en home

--- controllers/appController.js
+++ controllers/appController.js
@@ -1,33 +1,49 @@
-import {Precio, Categoria, Propiedad} from '../models/index.js'
+import { Precio, Categoria, Propiedad } from "../models/index.js";
 
-const inicio = async (req, res)  => {
-    const [categorias, precios] = await Promise.all([
-        Categoria.findAll({raw:true}),
-        Precio.findAll({raw:true})
-    ])
-    res.render('inicio',{
-        pagina: 'Inicio',
-        categorias,
-        precios
+const inicio = async (req, res) => {
+  const [categorias, precios, casas, departamentos] = await Promise.all([
+    Categoria.findAll({ raw: true }),
+    Precio.findAll({ raw: true }),
+    Propiedad.findAll({
+      limit: 3,
+      where: {
+        categoriaId: 1,
+      },
+      include: [
+        {
+          model: Precio,
+          as: "precio",
+        }
+      ],
+      order: [["createdAt", "DESC"]]
+    }),
+    Propiedad.findAll({
+      limit: 3,
+      where: {
+        categoriaId: 2,
+      },
+      include: [
+        {
+          model: Precio,
+          as: "precio",
+        }
+      ],
+      order: [["createdAt", "DESC"]]
+    }),
+  ]);
+  res.render("inicio", {
+    pagina: "Inicio",
+    categorias,
+    precios,
+    casas,
+    departamentos,
+  });
+};
 
-    })
-}
+const categoria = (req, res) => {};
 
-const categoria = (req, res)  => {
+const noEncontrado = (req, res) => {};
 
-}
+const buscador = (req, res) => {};
 
-const noEncontrado = (req, res)  => {
-
-}
-
-const buscador = (req, res)  => {
-
-}
-
-export {
-    inicio,
-    categoria,
-    noEncontrado,
-    buscador
-}
+export { inicio, categoria, noEncontrado, buscador };
